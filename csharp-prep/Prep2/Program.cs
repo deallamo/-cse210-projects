3301c4eec044fec1cbc47de757184f449ff1d167
deallamo <deaangelamiole@gmail.com>
2023-09-17 22:44:22 +0800

    prep2 activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int gradePercent = int.Parse(userGrade);
+
+        string letter = "";
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a grade of {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+             Console.WriteLine("Unfortunately, you failed.");
+        }
+
     }
 }
